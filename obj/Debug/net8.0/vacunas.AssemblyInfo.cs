9a39ea2b5a284018c5a257cd14d3b883bd558c67
ErickRosero <es.roseroh@uea.edu.ec>
2025-02-23 22:02:20 -0500

    reporte borrado

--- obj/Debug/net8.0/vacunas.AssemblyInfo.cs
+++ obj/Debug/net8.0/vacunas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vacunas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7178cc422686529bf79c797d4a073a3186cdbc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("vacunas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vacunas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
